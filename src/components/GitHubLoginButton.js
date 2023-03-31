e568ee763064f277983cc00419363ae907ffb512
Jaromir Hamala <jaromir.hamala@gmail.com>
2023-03-31 23:44:36 +0200

    remove unused var

--- src/components/GitHubLoginButton.js
+++ src/components/GitHubLoginButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const GitHubLoginButton = ({ clientId }) => {
     const loginWithGithub = () => {
-        const scope = 'public_repo';
         const redirectUri = encodeURIComponent('https://gh-stars.info/auth/github/callback');
 
         const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
