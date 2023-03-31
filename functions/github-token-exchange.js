a8445936cfffed68a69e5fd721ac483a1f0fa325
Jaromir Hamala <jaromir.hamala@gmail.com>
2023-03-31 22:49:01 +0200

    another attempt to auth

--- functions/github-token-exchange.js
+++ functions/github-token-exchange.js
@@ -1,32 +1,60 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.handler = async function (event, context) {
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== "GET") {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Invalid request method" }),
+        };
     }
 
-    const { code } = JSON.parse(event.body);
+    const code = event.queryStringParameters.code;
+
+    if (!code) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Authorization code is missing" }),
+        };
+    }
 
     try {
-        const response = await axios.post('https://github.com/login/oauth/access_token', {
-            client_id: process.env.GITHUB_CLIENT_ID,
-            client_secret: process.env.GITHUB_CLIENT_SECRET,
-            code,
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
+        const response = await axios.post(
+            "https://github.com/login/oauth/access_token",
+            {
+                client_id: GITHUB_CLIENT_ID,
+                client_secret: GITHUB_CLIENT_SECRET,
+                code: code,
             },
-        });
+            {
+                headers: {
+                    Accept: "application/json",
+                },
+            }
+        );
 
+        const { access_token: accessToken, token_type: tokenType } = response.data;
+
+        if (!accessToken) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Failed to obtain access token" }),
+            };
+        }
+
+        // Redirect the user back to the app with the access token
         return {
-            statusCode: 200,
-            body: JSON.stringify(response.data),
+            statusCode: 302,
+            headers: {
+                Location: `https://gh-stars.info?access_token=${accessToken}`,
+            },
         };
     } catch (error) {
         return {
-            statusCode: error.response.status,
-            body: JSON.stringify(error.response.data),
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
         };
     }
 };
